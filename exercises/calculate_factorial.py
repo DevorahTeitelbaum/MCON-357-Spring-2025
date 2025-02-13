0f44b8d08954cb39b067e0ea6a06e28f5ed4d99a
DevorahTeitelbaum <dteitelb@student.touro.edu>
2025-02-13 16:12:50 -0500

    modified calculate_factorial to handle fractions gracefully

--- exercises/calculate_factorial.py
+++ exercises/calculate_factorial.py
@@ -1,20 +1,31 @@
 # implemented factorial_recursive method
 def factorial_recursive(number):
-    if number > 0:
-        return number * factorial_recursive(number-1)
-    elif number == 0:
-        return 1;
-
-def main():
-    print("Factorial Computation Using Recursion")
-    number = int(input("Enter a non-negative integer: "))
-
     # handle negative input
     while number < 0:
         number = int(input("Only positive values allowed. Enter a non-negative integer: "))
+    if number > 0:
+        return number * factorial_recursive(number - 1)
+    elif number == 0:
+        return 1;
 
-    # Call factorial_recursive method in the printline as answer
+
+def main():
+    print("Factorial Computation Using Recursion")
+    isBadInput = True
+    number = .5
+    while isBadInput:
+        try:
+            number = int(input("Enter a non-negative integer: "))
+            if number < 0:
+                print("Please enter a non-negative integer.")
+            else:
+                isBadInput = False
+        except ValueError:
+            print("Invalid input! Please enter a valid integer.")
+
+
     print("Factorial of " + str(number) + " is: " + str(factorial_recursive(number)))
+
 
 if __name__ == "__main__":
     main()
